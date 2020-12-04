77892c00454b4eabb9d0de82c9c189c9bb073da0
Merve Kavaklioglu <55034055+merfinn@users.noreply.github.com>
2020-12-04 22:26:04 +0100

    clean up

--- codeChallange4/Contents.swift
+++ codeChallange4/Contents.swift
@@ -23,16 +23,12 @@ func matches(for regex: String, in text: String) -> [String] {
 //pid:[0-9]{9}(\n|\s|$)
 
 
-
 let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
-
 let fileInput = try! String(contentsOf: url)
-
 let lines = fileInput.components(separatedBy: "\n\n")
 
 func countValidPasspords(inputList:[String]) ->[String] {
     var validatedList = [String]()
-    
     for item in inputList {
         if item.contains("hcl:") &&
             item.contains("ecl:") &&
@@ -43,13 +39,9 @@ func countValidPasspords(inputList:[String]) ->[String] {
             item.contains("iyr:") {
             validatedList.append(item)
         }
-
     }
-    
     return validatedList
 }
-"hgt:176cm\niyr:2013\nhcl:#fffffd ecl:amb\nbyr:2000\neyr:2034\ncid:89 pid:934693255"
-
 
 func countParseAndValidate(inputList: [String] ,regex: String) -> Int {
     var count = 0
@@ -60,21 +52,18 @@ func countParseAndValidate(inputList: [String] ,regex: String) -> Int {
             count += 1
         }
     }
-    
     return count
 }
 
 var regex = "(byr:(19[2-9][0-9]|200[0-2])(\\n|\\s|$))|(iyr:(201[0-9]|2020)(\\n|\\s|$))|(hcl:#[0-9a-f]{6}(\\n|\\s|$))|(eyr:(202[0-9]|2030)(\\n|\\s|$))|(hgt:(((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in))(\\n|\\s|$))|(ecl:(amb|blu|brn|gry|grn|hzl|oth))(\\n|\\s|$)|(pid:([0-9]{9})(\\n|\\s|$))"
 
 var validPasswords = countValidPasspords(inputList: lines)
-
-var validPasswordsCount = validPasswords.count
-var validRulesCount = countParseAndValidate(inputList: validPasswords, regex: regex)
-
+var validPasswordsCount = validPasswords.count //solution of part 1
+var validRulesCount = countParseAndValidate(inputList: validPasswords, regex: regex) //solution of part 2
 
 
 
-//one regex solution.
+//one regex solution.//solution of part 2 with just one regex
 
 var oneRegex =
     "(" +
@@ -91,5 +80,4 @@ var oneRegex =
     "{7}"
 
 let fileInput2 = try! String(contentsOf: url)
-
 var countValidatedWithOneRegex = matches(for: oneRegex, in: fileInput2).count
